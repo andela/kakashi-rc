9bc63b6913dc0e5f2d5c01f2a35b767dcbf6c825
Faith Mustapha <faith.mustapha@andela.com>
2017-05-10 14:14:04 +0100

    feature(productDetailSimple.js): add js file to get disqus comment from profile page [#144187589]

--- imports/plugins/included/product-detail-simple/client/templates/productDetailSimple.js
+++ imports/plugins/included/product-detail-simple/client/templates/productDetailSimple.js
@@ -1,15 +1,36 @@
-import { ProductDetailContainer, PublishContainer } from "../containers";
+/* eslint-disable max-len*/
+import { ProductDetailContainer } from "../containers";
+import { isRevisionControlEnabled } from "/imports/plugins/core/revisions/lib/api";
+import { Template } from "meteor/templating";
+import { ReactiveDict } from "meteor/reactive-dict";
 
 Template.productDetailSimple.helpers({
+  isEnabled() {
+    return isRevisionControlEnabled();
+  },
   PDC() {
     return ProductDetailContainer;
   }
 });
 
-Template.productDetailSimpleToolbar.helpers({
-  PublishContainerComponent() {
-    return {
-      component: PublishContainer
-    };
+Template.embedDisqus.onCreated(function () {
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    feed: {}
+  });
+});
+
+Template.embedDisqus.helpers({
+  testDisplay() {
+    return { disqus: "col-sm-12" };
+  },
+
+  addDisqusThread() {
+    const d = document;
+    s = d.createElement("script");
+    s.src = "https://krc-1.disqus.com/embed.js";
+    s.setAttribute("data-timestamp", +new Date());
+    (d.head || d.body).appendChild(s);
   }
+
 });
