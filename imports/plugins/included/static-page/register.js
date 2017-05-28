c78f0964639e3378297ffd44d49ca937f4446d3a
Faith Mustapha <faith.mustapha@andela.com>
2017-05-28 23:41:33 +0100

    feature(included/static-page): add static page route, registry and layout to file [#144187593]

---
+++ imports/plugins/included/static-page/register.js
@@ -0,0 +1,35 @@
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  label: "Static Page",
+  name: "krc-static-page",
+  icon: "fa fa-file-text-o",
+  autoEnable: true,
+  registry: [{
+    route: "/dashboard/static-page",
+    provides: "dashboard",
+    workflow: "corePagesWorkFlow",
+    name: "Static Page",
+    label: "Static Page",
+    description: "Add and Manage Static Pages",
+    icon: "fa fa-plus",
+    priority: 1,
+    container: "core",
+    template: "staticPageLayout"
+  }],
+  layout: [{
+    layout: "coreLayout",
+    workflow: "corePagesWorkFlow",
+    theme: "default",
+    enabled: true,
+    structure: {
+      template: "staticPageLayout",
+      layoutHeader: "layoutHeader",
+      layoutFooter: "layoutFooter",
+      notFound: "notFound",
+      dashboardHeader: "dashboardHeader",
+      dashboardControls: "dashboardControls",
+      adminControlsFooter: "adminControlsFooter"
+    }
+  }]
+});
